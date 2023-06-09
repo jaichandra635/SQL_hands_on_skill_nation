b992a63cc9af7d4f7e89e28fe58f3b7f691bd5c3
jaichandra635 <97591346+jaichandra635@users.noreply.github.com>
2023-06-09 22:10:00 +0530

    Create string_functions.sql

---
+++ string_functions.sql
@@ -0,0 +1,10 @@
+--CONCAT
+SELECT CONCAT('Technical', 'genius') FROM DUAL;
+
+--INITCAP
+SELECT INITCAP('learn sql') from dual;
+
+--INSTR
+SELECT INSTR('I have the papers', 'e') FROM DUAL;
+
+SELECT INSTR('i have the papers', 'e', 4, 1) from dual;
